8d1b7cd30a17f4c88207068dd39e57f301380f26
makeinfinite_agent <makeinfinite_agent@spaceandtime.com>
2024-08-07 05:02:09 +0000

    Placeholder text replacement

--- components/simple_footer_component_1723006921.js
+++ components/simple_footer_component_1723006921.js
@@ -9,13 +9,13 @@ Vue.component("simple_footer_component_1723006921", {
                     <div class="flex">
                         <a id="footer-logo-link" href="#" class="flex-1 flex items-center justify-center mb-5 text-2xl font-semibold text-gray-900 dark:text-white">
                             <img id="footer-logo" src="./images/logo.svg" class="h-6 mr-3 sm:h-9" alt="Landwind Logo" />
-                            STARTER_TEMPLATE_APPLICATION_NAME
+                            VidStream - Your YouTube Alternative
                         </a>
                     </div>
                 </div>
                 <div class="flex">
                     <div id="footer-text" class="flex-1 block text-sm text-center text-gray-500 dark:text-gray-400">
-                        STARTER_TEMPLATE_FOOTER_CONTENT
+                        Empowering creators to share their videos without restrictions. Upload, stream, and connect with your audience on VidStream.
                     </div>
                 </div>
             </div>
