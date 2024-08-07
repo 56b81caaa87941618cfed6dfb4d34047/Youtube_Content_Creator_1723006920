5263c9b339bd1c667aa8788ec887b1c3f8a032ad
makeinfinite_agent <makeinfinite_agent@spaceandtime.com>
2024-08-07 05:04:54 +0000

    Merge code from frontend

--- components/simple_footer_component_1723006921.js
+++ components/simple_footer_component_1723006921.js
@@ -2,13 +2,13 @@
 */
 Vue.component("simple_footer_component_1723006921", {
     template: `
-    <footer id="footer-section" class="flex-1 bg-gradient-to-r from-pink-400 to-purple-600 bg-opacity-50 backdrop-filter backdrop-blur-lg">
+    <footer id="footer-section" style="min-height: 350px" class="flex-1 bg-gradient-to-r from-pink-400 to-purple-600 bg-opacity-50 backdrop-filter backdrop-blur-lg">
             <div id="footer-container" class="max-w-screen-xl p-4 py-6 mx-auto lg:py-16 md:p-8 lg:p-10 rounded-lg shadow-lg">
                 <hr id="footer-divider" class="my-6 border-pink-300 sm:mx-auto lg:my-8">
                 <div id="footer-content" class="text-center">
                     <div class="flex">
                         <a id="footer-logo-link" href="#" class="flex-1 flex items-center justify-center mb-5 text-2xl font-semibold text-white hover:text-pink-200 transition duration-300">
-                            <img id="footer-logo" src="./images/logo.svg" class="h-6 mr-3 sm:h-9" alt="Landwind Logo" />
+                            <img id="footer-logo" src="https://makeinfinite-mentat-test.azurewebsites.net/get_image/tmp_zhvx446/logo.svg" class="h-6 mr-3 sm:h-9" alt="Landwind Logo" />
                             VidStream - Your YouTube Alternative
                         </a>
                     </div>
